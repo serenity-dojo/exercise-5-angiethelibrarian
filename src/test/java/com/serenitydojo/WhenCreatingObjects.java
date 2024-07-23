639914b1f37345a151b71d64180bf94a478f0726
Angelica Mora <angiethelibrarian@gmail.com>
2024-07-22 21:39:42 -0400

    Created a Pet class, updated Dog and Cat classes, Created a new Hamster class, added abstract method play() for each pet respectively.

--- src/test/java/com/serenitydojo/WhenCreatingObjects.java
+++ src/test/java/com/serenitydojo/WhenCreatingObjects.java
@@ -42,9 +42,7 @@ public class WhenCreatingObjects {
     public void whenADogBarks() {
         Dog fido = new Dog("Fido","Bone", 5);
 
-        String dogSound = "";
-        // TODO: implement a method in the Dog sound called makeNoise() that returns the sound a dog makes: "Woof", e.g.
-        dogSound = fido.makeNoise();
+        String dogSound = fido.makeNoise();
 
         Assert.assertEquals(dogSound, "Woof");
     }
@@ -53,7 +51,6 @@ public class WhenCreatingObjects {
     public void whenADogGetsFed() {
         Dog fido = new Dog("Fido","Bone", 5);
 
-        // TODO: implement a method in the Dog sound called feed() that sets the isFed variable to true:
          fido.feed();
 
         Assert.assertTrue(fido.isFed());
